daba156dc7e84d8d658fa6f51b5ca4d8df99ab9c
Juan Bernardo <jrivera@hugeinc.com>
2016-07-12 09:30:42 -0500

    getCurrency

--- app/js/getCurrency.js
+++ app/js/getCurrency.js
@@ -1,8 +1,14 @@
-export default function getCurrency(number) {
-  if (!number) return 0;
-  return number.toString().replace(/(\d{0,3})?(\d{3})?(\d{3})$/g, function(_, $1, $2, $3) {
-    if (!$1 && !$2 && $3) return $3;
-    if ( $1 && !$2 && $3) return $1 + '.' + $3;
-    if ( $1 &&  $2 && $3) return $1 + '.' + $2 + '.' + $3;
-  })
+// function getCurrency(number) {
+//   if (!number) return 0;
+//   return number.toString().replace(/(\d{0,3})?(\d{3})?(\d{3})$/g, function(_, $1, $2, $3) {
+//     if (!$1 && !$2 && $3) return $3;
+//     if ( $1 && !$2 && $3) return $1 + '.' + $3;
+//     if ( $1 &&  $2 && $3) return $1 + '.' + $2 + '.' + $3;
+//   })
+// }
+
+function getCurrency(number) {
+  return number = number + ' ', number.replace(/(\d)(?=(\d{3})+\s)/g, '$1.').trim();
 }
+
+export default getCurrency;
