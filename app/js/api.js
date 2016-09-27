e5e147e5954d5194f41bc627cc33567266720e8b
Juan Bernardo <jrivera@hugeinc.com>
2016-09-26 22:42:12 -0500

    Api unused function

--- app/js/api.js
+++ app/js/api.js
@@ -14,25 +14,11 @@ const firebaseApp = firebase.initializeApp(config);
 
 const auth = firebase.auth();
 const facebook = new firebase.auth.FacebookAuthProvider();
-
 const database = firebase.database();
 
 const api = {
   '@@router/LOCATION_CHANGE': routerLocationChange(database, auth)
 };
-
-function setFireBaseTasks(next, action, store) {
-  next(action);
-  const state = Object.assign({}, store.getState());
-
-  if (state.user.res) {
-    database.ref(`users/${state.user.res.uid}/tasks`).set(
-      Object.keys(state.tasks).reduce((a, b, i) => {
-        return a[i] = { name: b, data: state.tasks[b] }, a;
-      }, [])
-    );
-  }
-}
 
 const firebaseMiddleware = store => next => {
 
