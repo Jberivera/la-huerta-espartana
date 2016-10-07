f7617273d9e86153a5745af1293ef55a8809be39
Juan Bernardo <jrivera@hugeinc.com>
2016-10-06 22:22:20 -0500

    Cart simple

--- app/components/Cart/Cart.js
+++ app/components/Cart/Cart.js
@@ -18,16 +18,6 @@ class Cart extends Component {
     super(props);
   }
 
-  optionMenuHandler (e) {
-    const { target, currentTarget } = e;
-    if (target.tagName === 'LABEL') {
-      const activeLabel = currentTarget.querySelector(`.${css('cart--label-active')}`);
-      activeLabel.classList.remove(css('cart--label-active'));
-
-      target.classList.add(css('cart--label-active'));
-    }
-  }
-
   render () {
     const { cart, date, message } = this.props;
 
@@ -37,17 +27,10 @@ class Cart extends Component {
       <div className={ css('cart') }>
         <h1 className={ css('cart__header') }>Carrito</h1>
         <div className={ css('cart__message') }>Revise sus articulos y seleccione una fecha de entrega</div>
-        <div className={ css('cart__options-menu') } onClick={ this.optionMenuHandler }>
-          <label className={ css('cart__option-label', 'cart--label-active') } htmlFor="cart-menu-1">Articulos</label>
-          <label className={ css('cart__option-label') } htmlFor="cart-menu-2">Fecha de Entrega</label>
-          <label className={ css('cart__option-label') } htmlFor="cart-menu-3">Finalizar Pedido</label>
-        </div>
-        <input type="radio" id="cart-menu-1" name="cart-menu" className={ css('cart__option-radio') } defaultChecked />
-        <div className={ css('cart__option-wrapper') }>
+        <div className={ css('cart__section-wrapper', 'cart--list') }>
           <CartList cart={ cart } />
         </div>
-        <input type="radio" id="cart-menu-2" name="cart-menu" className={ css('cart__option-radio') } />
-        <div className={ css('cart__option-wrapper') }>
+        <div className={ css('cart__section-wrapper', 'cart--date') }>
           <DatePick date={ date } />
           <div className={ css('cart__date-picker') }>
             <div className={ css('cart__date-mobile') }>
@@ -56,8 +39,7 @@ class Cart extends Component {
             </div>
           </div>
         </div>
-        <input type="radio" id="cart-menu-3" name="cart-menu" className={ css('cart__option-radio') } />
-        <div className={ css('cart__option-wrapper') }>
+        <div className={ css('cart__section-wrapper', 'cart--order') }>
           <CartOrder />
         </div>
       </div>
