f06354781e615723566690f04fd9420061049b5a
Juan Bernardo <jrivera@hugeinc.com>
2016-10-08 21:54:15 -0500

    Cart styles changes

--- app/components/Cart/CartList.js
+++ app/components/Cart/CartList.js
@@ -12,24 +12,23 @@ function CartList ({ cart }) {
       <li className={ css('cart__item-list') }>
         <div className={ css('cart__product-name', 'cart--col') }>Producto</div>
         <div className={ css('cart__count', 'cart--col') }>Cantidad</div>
-        <div className={ css('cart__price-title', 'cart--col') }>Precio</div>
+        <div className={ css('cart__price', 'cart--col') }>Precio</div>
         <div className={ css('cart__total', 'cart--col') }>Total</div>
       </li>
       {
         cart.map((item, i) => {
           return (
             <li key={ i } className={ css('cart__item-list') }>
-              <div className={ css('cart__product-name', 'cart--col') }>{ item.productName }</div>
+              <div className={ css('cart__product-name', 'cart--col') }>{ `${item.productName} ${item.units}` }</div>
               <div className={ css('cart__count', 'cart--col') }>{ item.count }</div>
               <div className={ css('cart__price', 'cart--col') }>{ `${item.price.toString().replace(/(\d{3})$/g, '.$1')}` }</div>
-              <div className={ css('cart__units', 'cart--col') }>{ item.units }</div>
               <div className={ css('cart__total', 'cart--col') }>{ item.price * item.count }</div>
             </li>
           );
         })
       }
       <li className={ css('cart__item-list') }>
-        <div className={ css('cart__product-name', 'cart--col') }>Total</div>
+        <div className={ css('cart__total-title', 'cart--col') }>Total</div>
         <div className={ css('cart__total', 'cart--col', 'cart__total-account') }>{ `$ ${getTotal(cart)}` }</div>
       </li>
     </ul>
