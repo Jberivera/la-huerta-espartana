3d33582b4bd43fcfda723063ba82034f1f1c73c8
Juan Bernardo <jrivera@hugeinc.com>
2016-10-08 23:14:22 -0500

    Cart item thumbnails

--- app/components/Cart/CartList.js
+++ app/components/Cart/CartList.js
@@ -19,17 +19,42 @@ function CartList ({ cart }) {
         cart.map((item, i) => {
           return (
             <li key={ i } className={ css('cart__item-list') }>
-              <div className={ css('cart__product-name', 'cart--col') }>{ `${item.productName} ${item.units}` }</div>
-              <div className={ css('cart__count', 'cart--col') }>{ item.count }</div>
-              <div className={ css('cart__price', 'cart--col') }>{ `${item.price.toString().replace(/(\d{3})$/g, '.$1')}` }</div>
-              <div className={ css('cart__total', 'cart--col') }>{ item.price * item.count }</div>
+              <div className={ css('cart__product-name', 'cart--col') }>
+                <div className={ css('cart__group') }>
+                  <img className={ css('cart__item-thumbnail') } src={ item.imgUrl }></img>
+                  <span>{ `${item.productName} - ${item.units}` }</span>
+                </div>
+              </div>
+              <div className={ css('cart__count', 'cart--col') }>
+                <div className={ css('cart__group', 'cart--center') }>
+                  { item.count }
+                </div>
+              </div>
+              <div className={ css('cart__price', 'cart--col') }>
+                <div className={ css('cart__group', 'cart--right') }>
+                  { `${item.price.toString().replace(/(\d{3})$/g, '.$1')}` }
+                </div>
+              </div>
+              <div className={ css('cart__total', 'cart--col') }>
+                <div className={ css('cart__group', 'cart--right') }>
+                  { item.price * item.count }
+                </div>
+              </div>
             </li>
           );
         })
       }
       <li className={ css('cart__item-list') }>
-        <div className={ css('cart__total-title', 'cart--col') }>Total</div>
-        <div className={ css('cart__total', 'cart--col', 'cart__total-account') }>{ `$ ${getTotal(cart)}` }</div>
+        <div className={ css('cart__total-title', 'cart--col') }>
+          <div className={ css('cart__group') }>
+            Total
+          </div>
+        </div>
+        <div className={ css('cart__total', 'cart--col', 'cart__total-account') }>
+          <div className={ css('cart__group', 'cart--right') }>
+            { `$ ${getTotal(cart)}` }
+          </div>
+        </div>
       </li>
     </ul>
   );
