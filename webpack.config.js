21f085e374f9b59172d3f5bfd4052a951805d34b
Juan Bernardo <jrivera@hugeinc.com>
2016-10-10 21:38:44 -0500

    Postcss flexbugs-fixes andAutoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,12 @@ const common = {
         include: PATHS.app
       }
     ]
+  },
+  postcss: function () {
+    return [
+      require('postcss-flexbugs-fixes'),
+      require('autoprefixer')({ browsers: ['last 2 versions'] })
+    ];
   }
 };
 
