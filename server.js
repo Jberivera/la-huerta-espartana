64d4da1ceccca6c47f3724b3acdbc86900fe9695
Juan Bernardo <jrivera@hugeinc.com>
2016-11-04 12:36:52 -0500

    Server COMODO

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 
 const TEMPLATE = fs.readFileSync('./index.html', { encoding: 'utf8' })
                    .replace('#styles', '/dist/css/main.css');
+const COMODO = fs.readFileSync('./F0158D48347E1C0C779EE51A7420584D.txt', { encoding: 'utf8' });
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use('/dist', express.static('dist'));
 app.use('/assets', express.static('assets'));
+
+app.get('/F0158D48347E1C0C779EE51A7420584D.txt', function (req, res) {
+  res.send(COMODO);
+})
 
 app.get('*', function (req, res) {
   res.send(
@@ -17,5 +22,5 @@ app.get('*', function (req, res) {
 });
 
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Server listening on port ${PORT}!`);
 });
