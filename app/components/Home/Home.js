1e9c60458ae06a30681fb884a932aedbbe050969
Juan Bernardo <jrivera@hugeinc.com>
2016-11-06 13:44:43 -0500

    Https secure and Video sign up

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -11,19 +11,19 @@ const Home = () => {
 
   return (
     <div className={ css('home') }>
-      <Hero backgroundUrl="url(http://i.imgur.com/BxLwxuf.jpg)" />
+      <Hero backgroundUrl="url(https://i.imgur.com/BxLwxuf.jpg)" />
       <div className={ css('section-wrapper', 'home__main-wrapper') }>
         <StoryTelling
           title="¿Cómo Funciona?"
           boxColor="#308694"
           img={{
-            url: "http://imgur.com/IpgmMUi.gif"
+            url: "https://imgur.com/IpgmMUi.gif"
           }} />
         <StoryTelling
           title="Compras desde tu casa"
           boxColor="#36B1C5"
           img={{
-            url: "http://i.imgur.com/RMM1TxG.gif",
+            url: "https://i.imgur.com/RMM1TxG.gif",
             style: {
               maxWidth: "500px",
               height: "100vh",
@@ -35,13 +35,13 @@ const Home = () => {
           title="Pagas cuándo recibas tus compras"
           boxColor="#66CAC9"
           img={{
-            url: "http://i.imgur.com/q5FvfPm.jpg"
+            url: "https://i.imgur.com/q5FvfPm.jpg"
           }} />
         <StoryTelling
           title="Crea una cuenta, es fácil y seguro"
           boxColor="#588c73"
           video={{
-            url: "https://video.feoh1-1.fna.fbcdn.net/v/t43.1792-2/15010213_1787156664906518_3173772457110667264_n.mp4?efg=eyJybHIiOjE1MDAsInJsYSI6MTAyNCwidmVuY29kZV90YWciOiJzdmVfaGQifQ%3D%3D&rl=1500&vabr=209&oh=1f8f67936ba3e2f17e3b1b6ad4062e46&oe=581F274B"
+            url: "/assets/video/huerta_signup.mp4"
           }} />
       </div>
     </div>
