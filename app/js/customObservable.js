4a7139dc82387bb98ef48f429804c56c72ebddcb
Juan Bernardo <jrivera@hugeinc.com>
2017-04-03 23:02:12 -0500

    customObservable

---
+++ app/js/customObservable.js
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/zip';
+import 'rxjs/add/observable/zip';
+import 'rxjs/add/operator/buffer';
+import 'rxjs/add/operator/debounceTime';
+
+export default Observable;
